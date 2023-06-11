73d109d8d005f7badf2d65f27bfb6b7b769938ed
Ramez Al-Siddiq <ramez.sydney@gmail.com>
2023-06-11 13:06:43 +1000

    added a classname for the img container div

--- pages/components/BlogCard.js
+++ pages/components/BlogCard.js
@@ -4,7 +4,9 @@ export default function BlogPost({title, author, coverPhoto, datePublished, slug
     return (
         <div className={styles.card}>
             <Link>
-            
+                <div className={styles.imgContainer}>
+                    <img src={coverPhoto.url} alt="" />
+                </div>
             </Link>
         </div>
     )
