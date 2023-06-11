cd846239c14a39b2d5ef8875c555211202c0885d
Ramez Al-Siddiq <ramez.sydney@gmail.com>
2023-06-11 13:04:29 +1000

    imported link into blog.js

--- pages/components/BlogCard.js
+++ pages/components/BlogCard.js
@@ -1,7 +1,11 @@
+import Link from 'next/link';
+
 export default function BlogPost({title, author, coverPhoto, datePublished, slug}) {
     return (
         <div className={styles.card}>
+            <Link>
             
+            </Link>
         </div>
     )
 }
