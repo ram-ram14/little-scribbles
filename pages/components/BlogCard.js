0b00208cc776fbc0d313f13b36992a443058bde0
Ramez Al-Siddiq <ramez.sydney@gmail.com>
2023-06-12 16:51:05 +1000

    added styling

--- pages/components/BlogCard.js
+++ pages/components/BlogCard.js
@@ -12,7 +12,7 @@ export default function BlogPost({title, author, coverPhoto, datePublished, slug
             <div className={styles.text}>
                 <h2>{title}</h2>
                 <div className={styles.details}>
-                    <div style={styles.author}>
+                    <div className={styles.author}>
                         <img src={author.avatar.url} alt='' />
                         <h3>{author.name}</h3>
                     </div>
